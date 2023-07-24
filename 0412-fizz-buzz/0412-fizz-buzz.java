00fb985edbe3b51341635f0fa89f7f9f0bcf9c73
Cheng Wu <112827606+Cheng57@users.noreply.github.com>
2023-07-24 17:56:03 -0500

    Update 0412-fizz-buzz.java
    String concatenation approach

--- 0412-fizz-buzz/0412-fizz-buzz.java
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -1,17 +1,26 @@
 class Solution {
     public List<String> fizzBuzz(int n) {
         List<String> list = new ArrayList<>();
-        
+
         for (int i = 1; i <= n; i++){
-            if (i % 3 == 0 && i % 5 == 0) {
-                list.add("FizzBuzz");
-            } else if (i % 3 == 0){
-                list.add("Fizz");
-            } else if (i % 5 == 0){
-                list.add("Buzz");
-            } else{
-                list.add(String.valueOf(i));
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+
+            String currStr = "";
+
+            if (divisibleBy3){ 
+                currStr += "Fizz";
             }
+
+            if (divisibleBy5){
+                currStr += "Buzz";
+            }
+
+            if (currStr.isEmpty()){
+                currStr += String.valueOf(i);
+            }
+
+            list.add(currStr);
         }
         
         return list;
