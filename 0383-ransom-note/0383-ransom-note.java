b80330a2f3e6b7aec55d58db5704c5a1d56e847b
Cheng Wu <112827606+Cheng57@users.noreply.github.com>
2023-08-23 16:40:00 -0500

    Time: 12 ms (57.71%), Space: 44.4 MB (19.68%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,31 +1,18 @@
-import java.util.*;
-
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        ArrayList<Character> al1 = new ArrayList<>();
-        ArrayList<Character> al2 = new ArrayList<>();
-        
-        for(char c : ransomNote.toCharArray()){
-            al1.add(c);
-        }
-        
-        for(char c : magazine.toCharArray()){
-            al2.add(c);
-        }
-        
-        int count = 0;
-        
-        for(int i = 0; i< al1.size(); i++){
-            if(al2.indexOf(al1.get(i)) >= 0){
-                al2.remove(al1.get(i));
-                count++;
+    
+        for(int i = 0; i < ransomNote.length(); i++){
+            
+            char c = ransomNote.charAt(i);
+            int matchingIndex = magazine.indexOf(c);
+            
+            if(matchingIndex == -1){
+                return false;
             }
+            
+            magazine = magazine.substring(0, matchingIndex) +               magazine.substring(matchingIndex + 1);
         }
         
-        if(count == al1.size()){
-            return true;
-        }
-        
-        return false;
+        return true;
     }
 }
